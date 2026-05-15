9ec0516b6d5aef31e91de0485a06318f248b6b22
Akshay <singhakshaya2008@gmail.com>
2026-05-16 04:31:27 +0530

    done one more

---
+++ BASICS/Conditional-expression.py
@@ -0,0 +1,6 @@
+#1 line code for else if statement aka Ternary Operator
+#in for of X if condition else Y
+x = int(input("Enter the first integer:"))
+y = int(input("Enter the second integer:"))
+
+print("greater number is " ,x if x > y else y )
