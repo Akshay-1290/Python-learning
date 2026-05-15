57d9798da355f311e4647f5c0e9a54a3c16f5f80
Akshay <singhakshaya2008@gmail.com>
2026-05-16 03:42:00 +0530

    Master choice in statement

---
+++ BASICS/Logical-Operators.py
@@ -0,0 +1,7 @@
+#and, or ,not are the logical operator works as studied in 10th class and physics
+velocity = int(input("What is your speed:"))
+if velocity >= 0 and velocity < 30000000 :
+  print("you are not travling in time")
+   
+else:
+      print("Congratsss you just skipped the time loop")
