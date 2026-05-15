b22b5cfc36809e0e267b524468aaf063a14e6fd5
Akshay <singhakshaya2008@gmail.com>
2026-05-16 05:10:24 +0530

    string basics

---
+++ STRINGS/Common_functions.py
@@ -0,0 +1,5 @@
+text = input("Enter any string with any number of alpha or number or anythinggg..!!")
+ # for length of string 
+
+print("Length of the string is ", len(text))
+# indent is error comes when you give space before any statement which doesnt need it may be accidently 
