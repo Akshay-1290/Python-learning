cfd8f2a4d6b15c4a1520cce297157aac6aa9e3dc
Akshay <singhakshaya2008@gmail.com>
2026-05-16 03:23:27 +0530

    idk what to say

---
+++ BASICS/typecasting.py
@@ -0,0 +1,17 @@
+#typecasting reffers to change in format of variable like float to int or anything 
+#Explicity 
+n = 5
+print(n)
+n = float(n)
+print(n)
+# for any work like 
+n += 2.1
+print(n)
+print(round(n))
+#Implicity i.e convert automaticalyy thorough operation
+
+x = 5
+y = 2.5
+
+x = x/y
+print(x)
