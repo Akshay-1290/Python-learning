a9bfd7d7dfe87a7e073a926bff99ab79966d0720
Akshay <singhakshaya2008@gmail.com>
2026-05-16 02:24:14 +0530

    completing basics

---
+++ BASICS/variables.py
@@ -0,0 +1,20 @@
+# hashtag for adding comments 
+# defining is easy af
+array = "anyname"
+print("array")
+print(array)
+# we dont use " " or ' for printing any word or grp of word 
+num = 5
+floatt = 5.5
+print(num , floatt)
+print(f"The number is {num}")
+#Boolean is a variable used for else if statements may be for more idk now =True or False with T and F capital
+Sunday = True 
+if Sunday:
+
+  print("Holyy day")
+
+else: 
+
+  print ("You have to go to work ") 
+  #you have to use backspace to get statements under if and else statements
