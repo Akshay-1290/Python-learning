28e058e1c67aeb5678581ff5f3a71b7fc9093c23
Akshay <singhakshaya2008@gmail.com>
2026-05-16 03:09:43 +0530

    easy easyy

---
+++ BASICS/takinginputs.py
@@ -0,0 +1,5 @@
+take = input("what is first input:")
+print(f"first input is {take}")
+# without defining type of variable input saves as array
+take2 = int(input("which take is this input:"))
+print (f"yup this is take{take2}")
