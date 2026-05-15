6bce284ae10d35c63ff2e44e20506e99c84f62d3
Akshay <singhakshaya2008@gmail.com>
2026-05-16 03:56:44 +0530

    Master Choice in statement 2

---
+++ BASICS/If-Elif-Else.py
@@ -0,0 +1,10 @@
+velocity = int(input("What is your velocity?:"))
+
+if velocity < 0 :
+    print("You might be on smth strong!!")
+elif velocity >= 0 and velocity < 112000: 
+    print ("SAFE LIMIT but above 100 survival is tuff on ind road ")
+elif velocity > 112000:
+    print("Might end up having tea with alien or on some random planet")
+else :
+    print("just u know what u r doin nga")
