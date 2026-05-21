3e2f6da2ee102928d714b88b4bbdb553a738b9a8
Akshay <singhakshaya2008@gmail.com>
2026-05-21 22:18:03 +0530

    def3

---
+++ Function/keyword-arg.py
@@ -0,0 +1,7 @@
+#keyword arguments are fixed for specific place they are also placed in name 
+#simple exercise for keyword word argument 
+def name(middle,first_name,last_name):
+ print(f"You are {first_name} {middle} {last_name}   ")
+ return
+
+print(name("mid",last_name="last", first_name="first"))
